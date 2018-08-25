2ef58ed35c9dcb1b45b8959605b19028e28f141b
Jason Viger <jason.viger@gmail.com>
2018-08-25 07:22:48 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,8 +1,8 @@
 class UpdateStudents < ActiveRecord::Migration[5.1]
   def change
-    add_column :students do |t|
+    change_column( :students do |t|
       t.integer :grade
-      t.string :birthdate
+      t.string :birthdate)
     end
   end
 end
